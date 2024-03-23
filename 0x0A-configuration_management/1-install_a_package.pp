44a62db793d81b87d5cf32d6e65124e5ada4e134
HavallonTech <kestercode@gmail.com>
2024-03-23 04:30:09 +0100

    Config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
-# Using Puppet, install flask from pip3.
+# Manifest file to install flask
 
 package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-package {'Werkzeug 2.1.1':
-    ensure   => '2.1.1',
-    provider => 'pip3',
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
 }
