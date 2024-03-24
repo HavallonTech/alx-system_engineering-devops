ec8b3e963821be480237f928c9f30b37e40e6d10
HavallonTech <kestercode@gmail.com>
2024-03-24 16:30:05 -0700

    Puppet config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#using puppet to modify config file
+
+file{'etc/ssh/ssh_config':
+        ensure => present,
+	content => "#SSH client configuration
+    		host*
+		IdentityFile ~/.ssh/school
+		PasswordAuthentication no",
+}
