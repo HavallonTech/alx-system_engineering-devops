19b7befbe7afc5340739e016d8eb12521298f24a
HavallonTech <kestercode@gmail.com>
2024-04-02 22:30:23 -0700

    ALX

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,21 @@
 #automating puppet installation
 
-package {'nginx':
+package { 'nginx':
   ensure => installed,
 }
-file_line{'install':
+
+file_line { 'install':
   ensure => 'present',
-  path => '/etc/nginx/sites-enabled/default',
-  after => 'listen 80 default_server;',
-  line => 'rewrite ^/redirect_me https://youtube.com permanent;',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://youtube.com permanent;',
 }
-file {'/var/www/html/index.html':
+
+file { '/var/www/html/index.html':
   content => 'Hello World!',
 }
+
 service { 'nginx':
-  ensure => running,
-  require => package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
