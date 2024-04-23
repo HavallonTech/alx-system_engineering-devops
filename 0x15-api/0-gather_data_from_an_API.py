0d1539eb65f9b6e3b0c72a39523595ebe050ccc5
HavallonTech <kestercode@gmail.com>
2024-04-23 21:55:47 +0100

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+ returns information about his/her TODO list progress from an API
+"""
 
 import requests
 import sys
@@ -6,15 +9,16 @@ import sys
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     emp_id = sys.argv[1]
-    user_response = request.get(url + "usrs/{}".format(emp_id))
+    user_response = requests.get(url + "users/{}".format(emp_id))
     user = user_response.json()
-    parameters = {"userid": emp_id}
-    todos_response = requests.get(url + "todos".params=parameters)
+    params = {"userid": emp_id}
+    todos_response = requests.get(url + "todos", params=params)
     todos = todos_response.json()
     completed = []
     for data in todos:
         if data.get("completed") is True:
             completed.append(data.get("title"))
-    print("Employee {} is done with tasks({}/{}".format(user.get("name").len(complete). len(todos)))
+    print("Employee {} is done with tasks({}/{}".format(user.get("name"),
+          len(completed), len(todos)))
     for complete in completed:
-        print("\t {}.format(complete))
+        print("\t {}".format(complete))
