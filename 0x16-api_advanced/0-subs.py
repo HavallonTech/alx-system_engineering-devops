27222da9e6d41554b60853929de491c63991ffd1
HavallonTech <kestercode@gmail.com>
2024-05-10 01:04:46 +0100

    ALX

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid 
+subreddit is given, the function should return 0.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agents': 'Any Name'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 404:
+        return
+    else:
+        subredit_data = response.json().get('data')
+        return subredit_data.get(subscribers)
