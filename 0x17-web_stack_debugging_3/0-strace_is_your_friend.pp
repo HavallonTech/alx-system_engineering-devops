9fb88799405e9db14ac617843075247e6d4dc22d
kester <132357927+HavallonTech@users.noreply.github.com>
2024-05-19 20:08:48 +0100

    Delete 0x17-web_stack_debugging_3 directory
    Ok deleted

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Fixing an apache file setting in a php environment
-
-exec{ 'fix-wordpres':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
-}
